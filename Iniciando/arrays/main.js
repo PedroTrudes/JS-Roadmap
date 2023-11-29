11ef85c2afa9790f765609f0d8ae6747f4665417
PedroTrudes <pedro.jtrudes@gmail.com>
2023-11-28 21:23:36 -0300

    inserindo e removendo infos de um array com (pop,push,unshift,shift)

--- Iniciando/arrays/main.js
+++ Iniciando/arrays/main.js
@@ -3,7 +3,7 @@ const arr = [];
 const obg = {};
 
 const produtos = [
-    {id: 1, nome: 'pedro', valor: 200, categoria: 'limpeza'},
+    {id: 1, nome: 'pedro', valor: 200, categoria: 'alimento'},
     {id: 10, nome: 'carol', valor: 200, categoria: 'limpeza'},
     {id: 5, nome: 'viviane', valor: 200, categoria: 'alimento'},
     {id: 8, nome: 'Rosa', valor: 200, categoria: 'limpeza'},
@@ -14,13 +14,25 @@ const alimentos = produtos
 .map(produto => produto.nome);//usando dois metodos juntos filter e map
 
 console.log(alimentos)
+alimentos.pop()
+console.log(alimentos)
+
 
 const ids = produtos.map(produto => produto.id);
 const nomes = produtos.map(produto => produto);
 
 console.log(ids);
-console.log(nomes)
+console.log(nomes);
+nomes.pop();// rovendo um arrya de objetos é o mesmo estilo;
+console.log(nomes);
 
 const numeros = [1, 2, 3, 4, 5];
 const duplicados = numeros.map(x => x *2);// retornando um array diferente
-console.log(duplicados)
+
+numeros.push(98);//inserindo no final do array
+numeros.unshift("Pedro") //inserindo no inicio do array
+console.log(numeros)
+numeros.pop()//removendo o ultimo array
+numeros.shift()//removendo o primeiro
+console.log(numeros)
+
